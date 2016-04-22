48395f1c4dfb8318699f704920de52541f5a3475
Juan Alberto Uribe Otero <juanalberto@kogimobile.com>
2016-04-22 15:03:15 -0500

    Initial Commit

---
+++ KeyboardResize/KeyboardResize.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardResize.h
+//  KeyboardResize
+//
+//  Created by Juan Alberto Uribe Otero on 4/22/16.
+//  Copyright © 2016 Kogi Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyboardResize.
+FOUNDATION_EXPORT double KeyboardResizeVersionNumber;
+
+//! Project version string for KeyboardResize.
+FOUNDATION_EXPORT const unsigned char KeyboardResizeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardResize/PublicHeader.h>
+
+
